38ef28ce3fc5a8a764b3b60f323e237c588fc8b3
ftonioloviemar <163175127+ftonioloviemar@users.noreply.github.com>
2024-03-16 14:47:31 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,38 @@
-if [ "$OSTYPE" = "linux-gnu" ]; then
-        echo "linux"
+so=$(uname -a | tr '[:upper:]' '[:lower:]')
+
+case "$so" in
+    *"debian"* )
+        echo "debian"
+        ;;
+    *"truenas"* )
+        echo "truenas"
+        ;;
+    *"pfsense"* )
+        echo "pfsense"
+        ;;
+    *"el6"* )
+        echo "el6"
         yum -y install git
-        
-elif [ "$OSTYPE" = "FreeBSD" ]; then
-        echo "freebsd pfsense"
-elif [ "$OSTYPE" = "freebsd12.0" ]; then
-        echo "freebsd truenas"
-else
-        echo "unknow operation system"
-fi
+        ;;
+    *"el7"* )
+        echo "el7"
+        yum -y install git
+        ;;
+    *"el8"* )
+        echo "el8"
+        yum -y install git
+        ;;
+    *"el9"* )
+        echo "el9"
+        yum -y install git
+        ;;
+    * )
+        echo "Outro SO não previsto"
+        ;;
+esac
+
+cd /opt
+rm -fdr halter
+git clone https://ftonioloviemar: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@github.com/ftonioloviemar/halter.git
+cd halter
+source install.sh
